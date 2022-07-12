147cdff519a8fca2368976ec5d0a7bce5d2a47c2
Brant Han <sghan@jcehrlich.local>
2022-07-12 09:55:26 -0700

    attempt 15% tip

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ const eighteenValue = document.getElementById("18pct");
 const twentyValue = document.getElementById("20pct");
 const btn = document.getElementById("calculate");
 
+let fifteenValueFinal = grandTotalValue.innerHTML * 0.15;
+
 
 btn.addEventListener("click", function () {
+   
     grandTotalValue.setAttribute("value", grandTotalValue.innerHTML);
     console.log(grandTotalValue.value);
+    fifteenValue.setAttribute("value", fifteenValueFinal);
+    console.log(fifteenValueFinal);
+    //fifteenValue.innerHTML = fifteenValue.value;
 });
