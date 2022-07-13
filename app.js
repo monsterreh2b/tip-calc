9805943a6dafaa372ff8db110922701b40d3693b
Brant Han <sghan@jcehrlich.local>
2022-07-13 09:32:40 -0700

    italics

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
-const grandTotalValue = document.getElementById("grandtotal-06212022");
-const fifteenValue = document.getElementById("15pct");
-const eighteenValue = document.getElementById("18pct");
-const twentyValue = document.getElementById("20pct");
-const btn = document.getElementById("calculate");
+ 
 
-let fifteenValueFinal = grandTotalValue.innerHTML * 0.15;
+
+//const eighteenValue = document.getElementById("18pct");
+//const twentyValue = document.getElementById("20pct");
+let btn = document.getElementById("calculate");
+
+//let fifteenValueTemp = 2;
 
 
 btn.addEventListener("click", function () {
+    let grandTotal = document.getElementById("grandtotal-06212022");
+    let grandTotalValue = document.getElementById("grandtotal-06212022").value;
+    grandTotal.setAttribute("value", grandTotalValue);
+ 
+    let tipFifteen = grandTotalValue * 0.15;
+
+    //console.log(tipFifteen);
+
+    document.getElementById("15pct").innerHTML = tipFifteen;
+
+    //document.getElementById("15pct").innerHTML = 
+    
+    //let tipFifteen = document.getElementById("15pct").value = grandTotalValue * 0.15;
+    //console.log(tipFifteen);
+    
+    //grandTotalValue.setAttribute("value", grandTotalValue.innerHTML);
+    //console.log(grandTotalValue);
+    //console.log(grandTotalValue * 0.15);
+    //fifteenValueTemp = grandTotalValue.value * 0.15;
+    //console.log(fifteenValueTemp);
    
-    grandTotalValue.setAttribute("value", grandTotalValue.innerHTML);
-    console.log(grandTotalValue.value);
-    fifteenValue.setAttribute("value", fifteenValueFinal);
-    console.log(fifteenValueFinal);
     //fifteenValue.innerHTML = fifteenValue.value;
-});
+})
