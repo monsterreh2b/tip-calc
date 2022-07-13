077e7d39cffa12f77673a12d72a48e7ace2cf7f4
Brant Han <sghan@jcehrlich.local>
2022-07-13 16:00:52 -0700

    calc percentage tips

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
  
 
-
 //const eighteenValue = document.getElementById("18pct");
 //const twentyValue = document.getElementById("20pct");
 let btn = document.getElementById("calculate");
@@ -10,25 +9,20 @@ let btn = document.getElementById("calculate");
 
 btn.addEventListener("click", function () {
     let grandTotal = document.getElementById("grandtotal-06212022");
-    let grandTotalValue = document.getElementById("grandtotal-06212022").value;
     grandTotal.setAttribute("value", grandTotalValue);
+
+    let grandTotalValue = document.getElementById("grandtotal-06212022").value;
+    
  
     let tipFifteen = grandTotalValue * 0.15;
+    let tipEighteen = grandTotalValue * 0.18;
+    let tipTwenty = grandTotalValue * 0.20;
 
-    //console.log(tipFifteen);
+   
 
     document.getElementById("15pct").innerHTML = tipFifteen;
+    document.getElementById("18pct").innerHTML = tipEighteen;
+    document.getElementById("20pct").innerHTML = tipTwenty;
 
-    //document.getElementById("15pct").innerHTML = 
     
-    //let tipFifteen = document.getElementById("15pct").value = grandTotalValue * 0.15;
-    //console.log(tipFifteen);
-    
-    //grandTotalValue.setAttribute("value", grandTotalValue.innerHTML);
-    //console.log(grandTotalValue);
-    //console.log(grandTotalValue * 0.15);
-    //fifteenValueTemp = grandTotalValue.value * 0.15;
-    //console.log(fifteenValueTemp);
-   
-    //fifteenValue.innerHTML = fifteenValue.value;
 })
