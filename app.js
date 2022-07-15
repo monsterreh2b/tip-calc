3b7e06707b89d6ce752d9364f302a78bad09b6be
Brant Han <sghan@jcehrlich.local>
2022-07-15 09:59:50 -0700

    calc tip works

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
+
+
 let grandTotal = document.getElementById("grandtotal-06212022");
-grandTotal.setAttribute("value", grandTotalValue);
 
-let grandTotalValue = document.getElementById("grandtotal-06212022").value;
-
-
-//const eighteenValue = document.getElementById("18pct");
-//const twentyValue = document.getElementById("20pct");
+console.log(grandTotal);
 let btn = document.getElementById("calculate");
-
-//let fifteenValueTemp = 2;
+let grandTotalConverted = 0;
 
 
 btn.addEventListener("click", function () {
 
+    console.log(grandTotal.value);
+   grandTotalConverted = Number(grandTotal.value);
+   console.log(grandTotalConverted + 20);
     
+   let tipFifteen = grandTotalConverted * 0.15;
+   let tipEighteen = grandTotalConverted * 0.18;
+   let tipTwenty = grandTotalConverted * 0.20;
 
- 
-    let tipFifteen = grandTotalValue * 0.15;
-    let tipEighteen = grandTotalValue * 0.18;
-    let tipTwenty = grandTotalValue * 0.20;
-
-   
+   console.log(tipFifteen);
+   console.log(tipEighteen);
+   console.log(tipTwenty);
 
     document.getElementById("15pct").innerHTML = tipFifteen;
     document.getElementById("18pct").innerHTML = tipEighteen;
     document.getElementById("20pct").innerHTML = tipTwenty;
 
-    
-})
+
+});
