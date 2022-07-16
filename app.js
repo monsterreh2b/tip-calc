45621d163aa75a049f9ba0cef2d4d8e6d6d99921
Brant Han <sghan@jcehrlich.local>
2022-07-16 14:43:05 -0700

    round to 2 decimals

--- app.js
+++ app.js
@@ -14,16 +14,19 @@ btn.addEventListener("click", function () {
    console.log(grandTotalConverted + 20);
     
    let tipFifteen = grandTotalConverted * 0.15;
+   let tipFifteenFinal = tipFifteen.toFixed(2);
    let tipEighteen = grandTotalConverted * 0.18;
+   let tipEighteenFinal = tipEighteen.toFixed(2);
    let tipTwenty = grandTotalConverted * 0.20;
+   let tipTwentyFinal = tipTwenty.toFixed(2);
 
-   console.log(tipFifteen);
-   console.log(tipEighteen);
-   console.log(tipTwenty);
+   console.log(tipFifteenFinal);
+   console.log(tipEighteenFinal);
+   console.log(tipTwentyFinal);
 
-    document.getElementById("15pct").innerHTML = tipFifteen;
-    document.getElementById("18pct").innerHTML = tipEighteen;
-    document.getElementById("20pct").innerHTML = tipTwenty;
+   document.getElementById("15pct").innerHTML = tipFifteenFinal;
+   document.getElementById("18pct").innerHTML = tipEighteenFinal;
+   document.getElementById("20pct").innerHTML = tipTwentyFinal;
 
 
 });
